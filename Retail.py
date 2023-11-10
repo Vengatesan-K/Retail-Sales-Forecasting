9a80e58879a717a57d14fb5464467b2525e17b7d
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-11-10 09:31:05 +0530

    Update Retail.py

--- Retail.py
+++ Retail.py
@@ -48,35 +48,6 @@ def style_metric_cards(
         unsafe_allow_html=True,
     )
 
-
-with open("style1.css") as f:
-    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-#####################
-# Navigation
-
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://www.linkedin.com/in/vengatesan2612/" target="_blank">Retail Sales Forecast</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#predict-price">Price</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-""", unsafe_allow_html=True)
-
-
-
 st.markdown('''
 <h2 style="color:#1e90ff; margin-bottom: 0;">Predict Week Sales</h2>
 <hr style="border: 1px solid #1e90ff; background-color: #1e90ff; margin-top: 0;">
