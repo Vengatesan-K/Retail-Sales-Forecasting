245b162ab1e3d14fa00bb868fddbbdd448e2912a
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-11-13 23:01:03 +0530

    Update Retail.py

--- Retail.py
+++ Retail.py
@@ -18,36 +18,6 @@ st.write("""
 </div>
 """, unsafe_allow_html=True)
 
-def style_metric_cards(
-    background_color: str = "#FFF",
-    border_size_px: int = 1,
-    border_color: str = "#CCC",
-    border_radius_px: int = 5,
-    border_left_color: str = "#9AD8E1",
-    box_shadow: bool = True,
-):
-
-    box_shadow_str = (
-        "box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important;"
-        if box_shadow
-        else "box-shadow: none !important;"
-    )
-    st.markdown(
-        f"""
-        <style>
-            div[data-testid="metric-container"] {{
-                background-color: {background_color};
-                border: {border_size_px}px solid {border_color};
-                padding: 5% 5% 5% 10%;
-                border-radius: {border_radius_px}px;
-                border-left: 0.5rem solid {border_left_color} !important;
-                {box_shadow_str}
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
 st.markdown('''
 <h2 style="color:#1e90ff; margin-bottom: 0;">Predict Week Sales</h2>
 <hr style="border: 1px solid #1e90ff; background-color: #1e90ff; margin-top: 0;">
